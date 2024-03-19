731cf314b9744a7a25305c0e48ad8ec3b4533bc6
MaksymDr <makc2708@gmail.com>
2024-03-19 17:06:37 -0400

    day14 added tasks

--- src/day16_practice_tasks/RemoveDuplicates.java
+++ src/day16_practice_tasks/RemoveDuplicates.java
@@ -6,11 +6,12 @@ public class RemoveDuplicates {
 
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
-        list.addAll(Arrays.asList(
+        List<Integer> list = new ArrayList<>(Arrays.asList(
                 20,15,10,20,30,10,20,15,0
         ));
-        /*Set<Integer> set = new TreeSet<>(list);
+
+        /*
+        Set<Integer> set = new TreeSet<>(list);
         list.clear();
         list.addAll(set);
         System.out.println(list);
@@ -21,8 +22,8 @@ public class RemoveDuplicates {
 
     }
     public static void removeDuplicates(List<Integer> list){
-        List<Integer> uniqueElements = new ArrayList<>();
 
+        List<Integer> uniqueElements = new ArrayList<>();
         for (Integer each : list) {
             if (uniqueElements.contains(each)){
                 continue;
--- src/practice/Try.java
+++ src/practice/Try.java
@@ -1,5 +1,12 @@
 package practice;
 
-public class Try {                                                                                                                                                
+public class Try {
+
+    public static void main(String[] args) {
+
+        String str = "Hello";
+        System.out.println(str.toCharArray());
+
+    }
 
 }
